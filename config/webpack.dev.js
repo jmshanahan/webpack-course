ec45489686a5f6e3cc49b251ae4fab2cef5f0929
jmshanahan <comeraghsolutions@gmail.com>
2018-09-07 19:30:48 +0100

    Babel implementation complete

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -16,6 +16,16 @@ module.exports = {
     module:{
         rules:[
             {
+                test: /\.js$/,
+                use: [
+                    {
+                    loader: "babel-loader"
+                    }
+                ],
+                exclude: /node_modules/
+
+            },
+            {
                 test: /\.css$/,
                 use:[
                     {
--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+// require("babel-runtime/regenerator");
 require("./main.css");
-require("./index.html")
+require("./index.html");
+
+var a = async (args) => {
+
+    await console.log("Hello from the future");
+    console.log("Done")
+}
+a();
